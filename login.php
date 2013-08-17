6c0877c5b83ffe5c744297b66f05cde49a9a60b0
NeoAtlantis <rijndael@yeah.net>
2013-08-17 11:35:54 +0800

    add error message displaying

--- login.php
+++ login.php
@@ -9,6 +9,7 @@ $__FIREWALL
 # template logical data
 $template_show_warning = true;
 $template_tab = 'login';
+$template_message_id = 1; # defined in template.
 
 # login or register logic.
 switch($__IO->get('do',true)){
@@ -26,6 +27,7 @@ switch($__IO->get('do',true)){
 $__IO
     ->data('option_show_warning', $template_show_warning)
     ->data('option_tab', $template_tab)
+    ->data('option_alert_message', $template_message_id)
 ;
 
 $__IO->output_HTML('login');
