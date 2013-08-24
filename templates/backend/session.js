4746047d1bfb3f98394563b334e700d6a92dc261
NeoAtlantis <rijndael@yeah.net>
2013-08-25 00:47:26 +0800

    constructing a ajax

--- templates/backend/session.js
+++ templates/backend/session.js
@@ -1,3 +1,20 @@
 var session = {
     
+    data: {},
+
+    initialize: function(){
+        setTimeout('session.ajax()', 1500);
+    },
+
+    ajax: function(){
+        setTimeout('session.ajax()', 1500);
+        $.get({
+            url: 'ajax.php', 
+            success: session.ajaxHandler,
+        });
+    },
+
+    ajaxHandler: function(data, txtStatus, jqXHR){
+    },
+
 };
