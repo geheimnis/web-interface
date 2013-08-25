6d4f6284257aa0b37f5aac68b15e7750692e41bd
NeoAtlantis <rijndael@yeah.net>
2013-08-25 15:02:54 +0800

    using base template

---
+++ task.php
@@ -0,0 +1,13 @@
+<?php
+require(dirname(__FILE__) . "/_.php");
+
+$__FIREWALL
+    ->require_login()
+    ->declare_side('back')
+    ->apply();
+
+$task_manager = new TASK_MANAGER();
+
+$__IO
+    ->output_HTML('task')
+;
